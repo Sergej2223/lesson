a9812b19dd353294c1807b8fc3a658ccde3ab35a
Sergey Maslov <sergej.serzh-m@yandex.ru>
2020-12-21 02:35:51 +0300

    commit 9

--- src/main/java/org/maslov/less/dao/UserDAO.java
+++ src/main/java/org/maslov/less/dao/UserDAO.java
@@ -2,11 +2,11 @@ package org.maslov.less.dao;
 
 import org.maslov.less.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
-
 import java.util.List;
 ;
 
@@ -15,41 +15,38 @@ public class UserDAO {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    private Connection conn;
+
 
     public List<User> getAll() {
-        return jdbcTemplate.query("select * from users", (rs, rowNum) -> {
-            User user = new User();
-            user.setName(rs.getString(1));
-            user.setSurname(rs.getString(2));
-            user.setSurname(rs.getString(3));
-            return user;
-        });
+        return jdbcTemplate.query("select * from users", new BeanPropertyRowMapper<>(User.class)
+        );
+    }
+
+
+    public User getOne(String email) {
+        try {
+            Connection conn;
+            PreparedStatement ps = conn.prepareStatement("select * from users where email = ?");
+           ps.setString(1, email);
+           ResultSet rs = ps.executeQuery();
+           if (rs.next()) {
+                User user = new User();
+                user.setName(rs.getString(1 ));
+               user.setSurname(rs.getString(2 ));
+               user.setEmail(rs.getString(3 ));
+                return user;
+            }
+        } catch (SQLException igonred) {
+       }
+        return null;
+   }
+
+    public void add(User user) throws SQLException {
+        Connection conn;
+        PreparedStatement ps = conn.prepareStatement("insert into users values (?, ?, ?)" );
+       ps.setString(1, user.getName() );
+       ps.setString(2, user.getSurname() );
+       ps.setString(3, user.getEmail() );
+       ps.execute();
     }
 }
-
-    //public User getOne(String email) {
-      //  try {
-        //    PreparedStatement ps = conn.prepareStatement("select * from users where email = ?");
-         //   ps.setString(1, email);
-         //   ResultSet rs = ps.executeQuery();
-         //   if (rs.next()) {
-          //      User user = new User();
-              //  user.setName(rs.getString(1 ));
-             //   user.setSurname(rs.getString(2 ));
-             //   user.setEmail(rs.getString(3 ));
-            //    return user;
-        //    }
-     //   } catch (SQLException igonred) {
-     //   }
-    //    return null;
-  //  }
-
-   // public void add(User user) throws SQLException {
-    //    PreparedStatement ps = conn.prepareStatement("insert into users values (?, ?, ?)" );
-     //   ps.setString(1, user.getName() );
-     //   ps.setString(2, user.getSurname() );
-    //    ps.setString(3, user.getEmail() );
-    //    ps.execute();
-  //  }
-//}
